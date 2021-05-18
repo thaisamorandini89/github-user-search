7a46aa3115e4b9671e37cc0acbe7549e6d95dc38
unknown <thaisamorandini@gmail.com>
2021-05-18 10:31:27 -0300

    Create navbar

---
+++ src/core/components/Navbar/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import './styles.scss';
+
+const Navbar = () => (
+    <nav className="row bg-primary main-nav">
+        <div className="col-3">
+            <a href="link" className="nav-logo-text">
+                <h4>Bootcamp DevSuperior</h4>
+            </a>
+        </div>
+    </nav>
+)
+
+export default Navbar;
