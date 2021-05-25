8a4b78e7133d8ee0ac359731873b32c9cd815c38
unknown <thaisamorandini@gmail.com>
2021-05-25 16:38:32 -0300

    Integration with backend

---
+++ src/core/types/profile.ts
@@ -0,0 +1,8 @@
+export type Profile = {
+    id: number;
+    company: string;
+    blog:string;
+    location:string;
+    created_at: Date;
+    avatar_url:string;
+}
--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,50 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchCard from './SearchCard';
-import { ReactComponent as ImageProfile} from '../../core/assets/images/profile.svg';
 import './styles.scss';
+import { Profile } from '../../core/types/profile';
+import { makeRequest } from '../../core/utils/request';
 
-const Search = () => (
-    <div>
-        <SearchCard title="Encontre um perfil Github">
-            <div className="row">
-                <div className="col-12">
-                    <input type="text" className="form-control" placeholder="Usuário Github"/>
+const Search = () => {
+
+    const [profile, setProfile] = useState<Profile>();
+
+    console.log(profile);
+
+    useEffect(() => {
+        makeRequest({ url: '/thaisamorandini89'})
+        .then(response => setProfile(response.data));
+    }, [])
+
+    return (
+        <div>
+            <SearchCard title="Encontre um perfil Github">
+                <div className="row">
+                    <div className="col-12">
+                        <input type="text" className="form-control" placeholder="Usuário Github" />
+                    </div>
+                </div>
+            </SearchCard>
+            <div className="card-base border-radius-10 detail-content">
+                <div className="col-2">
+                   <img src={profile?.avatar_url} alt={profile?.avatar_url} className="tamanho-imagem"/> 
+                </div>
+                <div className="col-9 profile-info-content">
+                    <p className="profile-info-title">Informações</p>
+                    <div >
+                        <ul>
+                            <li className="profile-info-box">
+                                <p className="title-box">Empresa:
+                               <span>{profile?.company}</span>
+                                </p>
+                            </li>
+                            <li className="profile-info-box">
+                                <p className="title-box">Website/blog:
+                               <span>{profile?.blog}</span>
+                                </p>
+                            </li>
+                            <li className="profile-info-box">
+                                <p className="title-box">Localidade:
+                               <span>{profile?.location}</span>
+                                </p>
+                            </li>
+                            <li className="profile-info-box">
+                                <p className="title-box">Membro desde:
+                               <span>{profile?.created_at}</span>
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </SearchCard>
-        <div className="card-base border-radius-10 detail-content">
-            <div className="col-2">
-                <ImageProfile className="tamanho-imagem"/>
-            </div>
-            <div className="col-9 profile-info-content">
-                <p className="profile-info-title">Informações</p>
-                <div >
-                    <ul>
-                        <li className="profile-info-box">
-                            <p className="title-box">Empresa:
-                               <span>@ZupIT</span> 
-                            </p>
-                        </li>
-                        <li className="profile-info-box">
-                            <p className="title-box">Website/blog:
-                               <span>https://thewashington.dev</span> 
-                            </p>
-                        </li>
-                        <li className="profile-info-box">
-                            <p className="title-box">Localidade:
-                               <span>Uberlândia</span> 
-                            </p>
-                        </li>
-                        <li className="profile-info-box">
-                            <p className="title-box">Membro desde:
-                               <span>19/10/2013</span> 
-                            </p>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>   
-    </div>
-)
+        </div>
+    )
+}
 
 export default Search;
